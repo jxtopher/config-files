4cce899a1c3863d655d4d66fbe832e9405e5eeeb
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2019-05-26 15:36:01 +0200

    Add files via upload

---
+++ categories-of-all-programs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Python 3.7
+
+#
+# @Author: *
+# @License: *
+# @Date: *
+# @Version: *
+#
+
+import os
+
+directories = ["Development", "Office", "Adminsys", "Science", "Multimedia", "Cryptography", "Browsers", "Computer-aided design"]
+
+
+for dir in directories:
+    try: 
+    os.makedirs(repertoire)
+    except OSError:
+        if not os.path.isdir(repertoire):
+            print("[-] Error")
