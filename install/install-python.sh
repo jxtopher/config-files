0033c9e0c09ce46f80b17d49d4b4bc6fa966e26d
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2020-11-13 12:46:18 +0100

    Update install-python.sh

--- install/install-python.sh
+++ install/install-python.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 
-sudo apt-get update
-sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+sudo -s
 
+## Installing the packages
+apt-get update
+apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+
+## Download, complie, install
 cd /tmp
 wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tgz
 tar xzf Python-3.9.0.tgz
@@ -10,4 +14,7 @@ tar xzf Python-3.9.0.tgz
 make -j 4
 sudo make altinstall
 
+exit
+
+## Check installation
 python3.9 --version
